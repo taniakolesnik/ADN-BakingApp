71d94ecf1bc55443bfc1141ce460acc9f473b098
Tania Kolesnik <kolesnik@gmail.com>
2018-08-27 20:31:28 +0100

    added restore instance state for steps list

--- app/src/main/java/uk/co/taniakolesnik/adn_bakingapp/DetailsActivity.java
+++ app/src/main/java/uk/co/taniakolesnik/adn_bakingapp/DetailsActivity.java
@@ -2,6 +2,7 @@ package uk.co.taniakolesnik.adn_bakingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,10 +16,16 @@ import uk.co.taniakolesnik.adn_bakingapp.Utils.StepsRecyclerViewAdapter;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private static final String TAG = DetailsActivity.class.getSimpleName();
+    private static final String RECIPE_KEY = "recipe_key";
     private Recipe mRecipe;
+    private Parcelable mListState;
+    private static final String LIST_STATE_KEY = "list_state";
+    @BindView(R.id.ingredients_textView)
+    TextView ingredients_textView;
+    @BindView(R.id.steps_recyclerView)
+    RecyclerView stepsRecyclerView;
 
-    @BindView(R.id.ingredients_textView) TextView ingredients_textView;
-    @BindView(R.id.steps_recyclerView) RecyclerView stepsRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +33,42 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         ButterKnife.bind(this);
 
-        Intent intent = getIntent();
-        mRecipe = (Recipe) intent.getSerializableExtra(getString(R.string.recipe_bundle));
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(RECIPE_KEY)) {
+                mRecipe = (Recipe) savedInstanceState.getSerializable(RECIPE_KEY);
+            }
+        } else {
+            Intent intent = getIntent();
+            mRecipe = (Recipe) intent.getSerializableExtra(getString(R.string.recipe_bundle));
+        }
 
-        // create ingridients list
+        // create ingredients list
         extractAndSetIngredientsList(mRecipe);
 
         // create step list
-        extractAndSetStepsList(mRecipe);
+        extractAndSetStepsList(mRecipe, savedInstanceState);
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(RECIPE_KEY, mRecipe);
+        mListState = stepsRecyclerView.getLayoutManager().onSaveInstanceState();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState!=null){
+            mListState = savedInstanceState.getParcelable(LIST_STATE_KEY);
+        }
     }
 
     private void extractAndSetIngredientsList(Recipe recipe) {
         ArrayList<Ingredient> ingredients = (ArrayList<Ingredient>) recipe.getIngredients();
         StringBuilder stringBuilder = new StringBuilder();
-        for (Ingredient ingredient : ingredients){
+        for (Ingredient ingredient : ingredients) {
             stringBuilder.append(" - ");
             stringBuilder.append(ingredient.getIngredient());
             stringBuilder.append(" - ");
@@ -50,9 +78,12 @@ public class DetailsActivity extends AppCompatActivity {
         ingredients_textView.setText(stringBuilder.toString());
     }
 
-    private void extractAndSetStepsList(Recipe recipe) {
+    private void extractAndSetStepsList(Recipe recipe, Bundle savedInstanceState) {
         ArrayList<Step> steps = (ArrayList<Step>) recipe.getSteps();
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        if (savedInstanceState!=null){
+            layoutManager.onRestoreInstanceState(mListState);
+        }
         stepsRecyclerView.setLayoutManager(layoutManager);
         StepsRecyclerViewAdapter adapter = new StepsRecyclerViewAdapter(getApplicationContext(), steps);
         stepsRecyclerView.setAdapter(adapter);
--- app/src/main/java/uk/co/taniakolesnik/adn_bakingapp/MainActivity.java
+++ app/src/main/java/uk/co/taniakolesnik/adn_bakingapp/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,7 +39,6 @@ public class MainActivity extends AppCompatActivity implements  LoaderManager.Lo
         mAdapter = new RecipesRecyclerViewAdapter(this, new ArrayList<Recipe>());
         if (savedInstanceState!=null){
             layoutManager.onRestoreInstanceState(mListState);
-            Log.i(TAG, "onCreate savedInstanceState not null");
         }
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(mAdapter);
@@ -49,7 +47,6 @@ public class MainActivity extends AppCompatActivity implements  LoaderManager.Lo
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        Log.i(TAG, "onSaveInstanceState");
         super.onSaveInstanceState(outState);
         mListState = recyclerView.getLayoutManager().onSaveInstanceState();
 
@@ -57,23 +54,19 @@ public class MainActivity extends AppCompatActivity implements  LoaderManager.Lo
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        Log.i(TAG, "onRestoreInstanceState");
         super.onRestoreInstanceState(savedInstanceState);
         if (savedInstanceState!=null){
-            Log.i(TAG, "onRestoreInstanceState savedInstanceState is not null");
             mListState = savedInstanceState.getParcelable(LIST_STATE_KEY);
         }
     }
 
     @Override
     public Loader<List<Recipe>> onCreateLoader(int id, Bundle args) {
-        Log.i(TAG, "onCreateLoader");
         return new RecipeAsyncTaskLoader(this);
     }
 
     @Override
     public void onLoadFinished(Loader<List<Recipe>> loader, List<Recipe> data) {
-        Log.i(TAG, "onLoadFinished");
         mAdapter.updateData(data);
     }
 
