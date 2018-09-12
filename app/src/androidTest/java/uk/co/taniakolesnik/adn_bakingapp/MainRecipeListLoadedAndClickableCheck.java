f2de4b50d0bf79803d59f693e809ce5b91867bbd
Tania Kolesnik <kolesnik@gmail.com>
2018-09-12 12:54:03 +0100

    added more espresso test steps

--- app/src/androidTest/java/uk/co/taniakolesnik/adn_bakingapp/MainRecipeListLoadedAndClickableCheck.java
+++ app/src/androidTest/java/uk/co/taniakolesnik/adn_bakingapp/MainRecipeListLoadedAndClickableCheck.java
@@ -8,11 +8,18 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by tetianakolesnik on 06/09/2018.
@@ -33,8 +40,27 @@ public class MainRecipeListLoadedAndClickableCheck {
         // check if recipe name is clickable
         onView(withId(R.id.recipes_recyclerView))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, TestViewAction.clickRecycletItemWithId(R.id.recipes_name)));
+
+        // click on Ingredients tab
+        onView(allOf(withText("Ingredients"),
+                isDescendantOfA(withId(R.id.tabLayout)))).perform(click());
+
+        // check Ingredients first item is not empty
+        onData(anything())
+                .inAdapterView(allOf(withId(R.id.ingredients_listView), isCompletelyDisplayed()))
+                .atPosition(0).check(matches(not(withText(""))));
+
+        // back to Instructions tab
+        onView(allOf(withText("Instructions"),
+                isDescendantOfA(withId(R.id.tabLayout)))).perform(click());
+
+        // click on the first Instructions step
+        onView(withId(R.id.steps_recyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, TestViewAction.clickRecycletItemWithId(R.id.step_short_description_textView)));
+
+        // check step full description is loaded
+        onView(withId(R.id.step_description_textView))
+                .check(matches(not(withText(""))));
+
     }
-
-
-
 }
